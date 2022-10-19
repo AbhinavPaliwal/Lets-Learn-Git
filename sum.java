e4f951768d34cba6a97c758884f463c64d4a915f
Abhinav Paliwal <abhinavpaliwal95@gmail.com>
2022-10-19 12:47:44 +0530

    initial commit

--- sum.java
+++ sum.java
@@ -3,7 +3,8 @@ public class sum{
     public static void main(String args[]) {
         int a = 5;
         int b = 6;
+        int c = 7;
 
-        System.out.println("The sum :" + a+b);
+        System.out.println("The sum :" + a+b+c);
     }
 }
